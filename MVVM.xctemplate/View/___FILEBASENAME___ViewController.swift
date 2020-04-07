ca100bd96ab26a851490821c528a076244e5a52b
Rajneesh Kumar <kumarrajnees@gmail.com>
2020-04-07 15:40:13 +0530

    Create generic file

--- MVVM.xctemplate/View/___FILEBASENAME___ViewController.swift
+++ MVVM.xctemplate/View/___FILEBASENAME___ViewController.swift
@@ -4,62 +4,27 @@
 import UIKit
 
 class ___VARIABLE_productName:identifier___ViewController: UIViewController {
-    
+    //MARK:- Properties and Outlets
     // Add here outlets
     
-    // Add here your view model
+    // Your view model
     lazy var viewModel: ___VARIABLE_productName:identifier___ViewModel = {
         return ___VARIABLE_productName:identifier___ViewModel()
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
         initViewModel()
     }
-    
+    //MARK:- Private functions
     func setupUI() {
         // Add here the setup for the UI
     }
     
+    // ViewModal callbacks
     func initViewModel() {
         // Add here the setup for the ViewModel
-        
-        // Setup for closure1
-        viewModel.closure1 = { [weak self] () in
-            DispatchQueue.main.async {
-                // Do something
-            }
-        }
-        
-        // Setup for closure2
-        viewModel.closure2 = { [weak self] () in
-            DispatchQueue.main.async {
-                // Do something
-            }
-        }
-        
-        // Setup for updateLoadingStatusClosure
-        viewModel.updateLoadingStatusClosure = { [weak self] () in
-            DispatchQueue.main.async {
-                let isLoading = self?.viewModel.isLoading ?? false
-                if isLoading {
-                    self?.activityIndicator.startAnimating()
-                    
-                    // Do something else, for example hide some UI controls.
-                    
-                } else {
-                    self?.activityIndicator.stopAnimating()
-                    
-                    // Do something else, for example show some UI controls.
-                }
-            }
-        }
-        
-        // Populate ViewModel:
-        // For example:
-        // viewModel.fetchData()
     }
     
     // MARK: - Navigation
